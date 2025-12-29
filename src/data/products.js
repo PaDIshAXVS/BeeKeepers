5243be634ff7fe321de25b7c89eabbd3520e64ac
PaDIshAXVS <kirsem797@gmail.co>
2025-12-29 17:12:22 +0300

    Добавлен каталог

---
+++ src/data/products.js
@@ -0,0 +1,21 @@
+// src/data/products.js
+export const products = [
+  {
+    title: "Перга",
+    price: 600,
+    img: "/images/Perga.jpeg",
+    desc: "Натуральная перга с нашей пасеки, 0,5 л."
+  },
+  {
+    title: "Липовый",
+    price: 550,
+    img: "/images/honey.jpg",
+    desc: "Липовый мёд, 0,5 л."
+  },
+  {
+    title: "Воск",
+    price: 500,
+    img: "/images/Vosk.jpg",
+    desc: "Натуральный воск, 100 г."
+  }
+];
